bf2be01b0181453a7418f4c536c0dfbc1e7c1faf
15000712803 <15000712803@139.com>
2017-11-13 10:05:08 +0800

    pager tab strip

---
+++ app/src/main/java/com/cai/newc/ui/fragment/HomeOneTabFragment.java
@@ -0,0 +1,15 @@
+package com.cai.newc.ui.fragment;
+
+import com.cai.newc.R;
+import com.cai.newc.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/11/13 0013.
+ */
+
+public class HomeOneTabFragment extends BaseFragment{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_test;
+    }
+}
