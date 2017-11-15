2fd5da6ef06f9139ad867932ef3a5906778c68ca
tmsui <tmsuiRunForever@163.com>
2017-11-15 16:48:29 +0800

    mineFragment_page_ui

---
+++ app/src/main/java/com/cai/newc/ui/activity/SettingActivity.java
@@ -0,0 +1,32 @@
+package com.cai.newc.ui.activity;
+
+import android.os.Message;
+
+import com.cai.newc.R;
+import com.cai.newc.base.BaseActivity;
+
+/**
+ * Created by Sunway on 2017/11/15.
+ */
+
+public class SettingActivity extends BaseActivity {
+    @Override
+    public int layout() {
+        return R.layout.page_setting;
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void onMessage(Message msg) {
+
+    }
+}
