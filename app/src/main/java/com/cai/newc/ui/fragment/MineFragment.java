d2673aefa29a63f6de9dcba3da7c79d116815785
tmsui <tmsuiRunForever@163.com>
2017-11-16 10:26:21 +0800

    local

--- app/src/main/java/com/cai/newc/ui/fragment/MineFragment.java
+++ app/src/main/java/com/cai/newc/ui/fragment/MineFragment.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import com.cai.newc.R;
 import com.cai.newc.base.BaseFragment;
+import com.cai.newc.ui.activity.PersonalActivity;
 import com.cai.newc.ui.activity.SettingActivity;
 
 import butterknife.Bind;
@@ -16,6 +17,8 @@ import butterknife.Bind;
 public class MineFragment extends BaseFragment{
     @Bind(R.id.ll_setting)
     LinearLayout setting;
+    @Bind(R.id.ll_mine)
+    LinearLayout mine;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_mine;
@@ -27,6 +30,12 @@ public class MineFragment extends BaseFragment{
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(),SettingActivity.class));
+            }
+        });
+        mine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),PersonalActivity.class));
             }
         });
     }
